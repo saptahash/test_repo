11c7c1c2bcdb88458a793bdcd56cb4bccf21af4a
Saptarshi Majumdar <saptarshi3.14@gmail.com>
2020-09-06 03:36:02 +0100

    correcting typo in installation

--- auto_test/usdata.r
+++ auto_test/usdata.r
@@ -1,6 +1,6 @@
 #!/usr/bin/Rscript
 
-if(!require(dpyl)) install.packages("dyplr")
+if(!require(dplyr)) install.packages("dplyr")
 library(dplyr)
 US_covid <- read.csv("https://raw.githubusercontent.com/OxCGRT/USA-covid-policy/master/data/OxCGRT_US_latest.csv")
 write.csv("./data/OxCGRT_US_latest.csv", US_covid)
