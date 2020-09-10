d8e0e5ad585c04cb3c2d63ad62661b555beecf16
Saptarshi Majumdar <saptahash@users.noreply.github.com>
2020-09-10 13:02:46 +0100

    Testing here package to find path

--- auto_test/usdata.r
+++ auto_test/usdata.r
@@ -1,6 +1,11 @@
 #!/usr/bin/Rscript
 
 if(!require(dplyr)) install.packages("dplyr")
+if(!require(here)) install.packages("here")
+
+library(here)
 library(dplyr)
-US_covid <- read.csv("https://raw.githubusercontent.com/OxCGRT/USA-covid-policy/master/data/OxCGRT_US_latest.csv")
-write.csv("./auto_test/data/OxCGRT_US_latest.csv", US_covid)
+
+here()
+#US_covid <- read.csv("https://raw.githubusercontent.com/OxCGRT/USA-covid-policy/master/data/OxCGRT_US_latest.csv")
+#write.csv(US_covid, "./auto_test/data/OxCGRT_US_latest.csv")
