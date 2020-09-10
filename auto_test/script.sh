87d561790056a7bf08160f0ed94af68fcd4052a6
Saptarshi Majumdar <saptarshi3.14@gmail.com>
2020-09-10 16:41:14 +0100

    Changing executables in script

--- auto_test/script.sh
+++ auto_test/script.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/bash
 
 chmod 755 shebang.r
-Rscript usdata.r
+Rscript '../oxcgrt/codes/OxCGRT_data_prep.r'
+#Rscript '../oxcgrt/codes/rollback_checklist.r'
+#Rscript '../oxcgrt/codes/rollback_dataviz.r'
