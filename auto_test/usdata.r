ec81337b6e967fe0a796387d6e5e9abfb236f771
Saptarshi Majumdar <saptahash@users.noreply.github.com>
2020-09-10 12:04:56 +0100

    Changing path of output

--- auto_test/usdata.r
+++ auto_test/usdata.r
@@ -3,4 +3,4 @@
 if(!require(dplyr)) install.packages("dplyr")
 library(dplyr)
 US_covid <- read.csv("https://raw.githubusercontent.com/OxCGRT/USA-covid-policy/master/data/OxCGRT_US_latest.csv")
-write.csv("./data/OxCGRT_US_latest.csv", US_covid)
+write.csv("./auto_test/data/OxCGRT_US_latest.csv", US_covid)
