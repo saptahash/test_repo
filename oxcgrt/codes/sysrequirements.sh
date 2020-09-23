550087ad21edb0b1a19b3f5a41586a14b7aed451
Saptarshi Majumdar <saptarshi3.14@gmail.com>
2020-09-23 23:21:48 +0100

    Error in sys file -- corrected

--- oxcgrt/codes/sysrequirements.sh
+++ oxcgrt/codes/sysrequirements.sh
@@ -4,4 +4,4 @@ sudo apt install -y libudunits2-0 libudunits2-dev
 sudo apt install libgdal-dev
 Rscript -e 'install.packages("remotes")'
 Rscript -e 'remotes::install_cran(c("countrycode", "feather", "rgeos", "ggrepel",
-"ggthemes", "viridis", "tidyverse", "RcppRoll", "zoo", "haven","sf", "rnaturalearth", "rnaturalearthdata", "here"))
+"ggthemes", "viridis", "tidyverse", "RcppRoll", "zoo", "haven","sf", "rnaturalearth", "rnaturalearthdata", "here"))'
