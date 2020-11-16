0961042f82bd80464b4a5a6308d0904dfec66a34
Saptarshi Majumdar <saptahash@users.noreply.github.com>
2020-11-16 13:58:28 +0000

    Update sysrequirements.sh

--- mainrepo/sysrequirements.sh
+++ mainrepo/sysrequirements.sh
@@ -3,5 +3,5 @@ sudo apt-get install libcurl4-openssl-dev
 sudo apt install -y libudunits2-0 libudunits2-dev
 sudo apt install libgdal-dev
 Rscript -e 'install.packages("remotes")'
-Rscript -e 'remotes::install_cran(c("tidyverse", "devtools", "rgeos", "ggthemes", "zoo","sf", "rnaturalearth", "rnaturalearthdata","here", "scales", "lubridate","writexl","openxlsx","janitor"))'
+Rscript -e 'remotes::install_cran(c("ellipsis","tidyverse", "devtools", "rgeos", "ggthemes", "zoo","sf", "rnaturalearth", "rnaturalearthdata","here", "scales", "lubridate","writexl","openxlsx","janitor"))'
 Rscript -e 'devtools::install_github("tidyverse/dplyr")'
