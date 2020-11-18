f0cbafc45a66d14241d5f35fa4d8c14a9b0c9108
Saptarshi Majumdar <saptarshi3.14@gmail.com>
2020-11-18 15:55:50 +0000

    Adding system requirements and dependencies file

---
+++ regionalchanges/sysrequirements.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt-get install libcurl4-openssl-dev
+sudo apt install -y libudunits2-0 libudunits2-dev
+sudo apt install libgdal-dev
+Rscript -e 'install.packages("remotes")'
+Rscript -e 'remotes::install_cran(c("ggplot2", "readr", "lubridate", "stringr", "devtools"))'
+Rscript -e 'devtools::install_url("https://cran.r-project.org/src/contrib/Archive/dplyr/dplyr_1.0.1.tar.gz")'
