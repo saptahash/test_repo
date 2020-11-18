ecc8f495556908f8d0bd328b97429cbaca449c92
Saptarshi Majumdar <saptahash@users.noreply.github.com>
2020-11-18 17:41:08 +0000

    remove stringr

--- regionalchanges/sysrequirements.sh
+++ regionalchanges/sysrequirements.sh
@@ -3,5 +3,5 @@ sudo apt-get install libcurl4-openssl-dev
 sudo apt install -y libudunits2-0 libudunits2-dev
 sudo apt install libgdal-dev
 Rscript -e 'install.packages("remotes")'
-Rscript -e 'remotes::install_cran(c("ggplot2", "tidyr", "readr", "lubridate", "stringr", "devtools", "scales"))'
+Rscript -e 'remotes::install_cran(c("ggplot2", "tidyr", "readr", "lubridate", "devtools", "scales"))'
 Rscript -e 'devtools::install_url("https://cran.r-project.org/src/contrib/Archive/dplyr/dplyr_1.0.1.tar.gz")'
