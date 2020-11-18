bc33c2d23df48343b2a659694eee4d2843525215
Saptarshi Majumdar <saptahash@users.noreply.github.com>
2020-11-18 16:35:44 +0000

    Adding tidyr

--- regionalchanges/sysrequirements.sh
+++ regionalchanges/sysrequirements.sh
@@ -3,5 +3,5 @@ sudo apt-get install libcurl4-openssl-dev
 sudo apt install -y libudunits2-0 libudunits2-dev
 sudo apt install libgdal-dev
 Rscript -e 'install.packages("remotes")'
-Rscript -e 'remotes::install_cran(c("ggplot2", "readr", "lubridate", "stringr", "devtools"))'
+Rscript -e 'remotes::install_cran(c("ggplot2", "tidyr", "lubridate", "stringr", "devtools"))'
 Rscript -e 'devtools::install_url("https://cran.r-project.org/src/contrib/Archive/dplyr/dplyr_1.0.1.tar.gz")'
