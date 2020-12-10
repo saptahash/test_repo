9bba241008efbcc1f40ac5c0ff94ccf4bbe8cb15
Saptarshi Majumdar <saptahash@users.noreply.github.com>
2020-12-10 12:34:11 +0000

    check for devtools install

--- oxcgrt/codes/sysrequirements.sh
+++ oxcgrt/codes/sysrequirements.sh
@@ -2,5 +2,5 @@
 sudo apt-get install libcurl4-openssl-dev
 sudo apt install -y libudunits2-0 libudunits2-dev
 sudo apt install libgdal-dev
-Rscript -e 'install.packages("remotes")'
-Rscript -e 'remotes::install_cran(c("countrycode", "devtools", "feather", "rgeos", "ggrepel", "ggthemes", "viridis", "tidyverse", "RcppRoll", "zoo", "haven","sf", "rnaturalearth", "rnaturalearthdata", "here"))'
+Rscript -e 'install.packages("remotes", "devtools")'
+Rscript -e 'remotes::install_cran(c("countrycode", "feather", "rgeos", "ggrepel", "ggthemes", "viridis", "tidyverse", "RcppRoll", "zoo", "haven","sf", "rnaturalearth", "rnaturalearthdata", "here"))'
