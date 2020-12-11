eb7b05ab4a3f7f5752e275d325160b7aae82e161
Saptarshi Majumdar <saptahash@users.noreply.github.com>
2020-12-11 13:05:46 +0000

    Moving up devtools install outside install_cran

--- mainrepo/sysrequirements.sh
+++ mainrepo/sysrequirements.sh
@@ -2,7 +2,7 @@
 sudo apt-get install libcurl4-openssl-dev
 sudo apt install -y libudunits2-0 libudunits2-dev
 sudo apt install libgdal-dev
-Rscript -e 'install.packages("remotes")'
-Rscript -e 'remotes::install_cran(c("ellipsis","tidyverse", "devtools", "rgeos", "ggthemes", "zoo","sf", "rnaturalearth", "rnaturalearthdata","here", "scales", "lubridate","writexl","openxlsx","janitor"))'
+Rscript -e 'install.packages("remotes", "devtools")'
+Rscript -e 'remotes::install_cran(c("ellipsis","tidyverse", "rgeos", "ggthemes", "zoo","sf", "rnaturalearth", "rnaturalearthdata","here", "scales", "lubridate","writexl","openxlsx","janitor"))'
 Rscript -e 'devtools::install_url("https://cran.r-project.org/src/contrib/Archive/dplyr/dplyr_1.0.1.tar.gz")'
 Rscript -e 'devtools::install_github("tidyverse/tibble")'
