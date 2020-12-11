bba6162a4f2a65726cd7e846e930282c9b725cac
Saptarshi Majumdar <saptahash@users.noreply.github.com>
2020-12-11 22:01:03 +0000

    remove devtools and use remotes instead
    devtools is crashing after possible versioning clash with usethis update

--- mainrepo/sysrequirements.sh
+++ mainrepo/sysrequirements.sh
@@ -5,7 +5,7 @@ sudo apt install libgdal-dev
 #adding two additional installs to fix devtools bug
 apt-get -y build-dep libcurl4-gnutls-dev
 apt-get -y install libcurl4-gnutls-dev
-Rscript -e 'install.packages(c("remotes", "devtools"))'
+Rscript -e 'install.packages(c("remotes"))'
 Rscript -e 'remotes::install_cran(c("ellipsis","tidyverse", "rgeos", "ggthemes", "zoo","sf", "rnaturalearth", "rnaturalearthdata","here", "scales", "lubridate","writexl","openxlsx","janitor"))'
-Rscript -e 'devtools::install_url("https://cran.r-project.org/src/contrib/Archive/dplyr/dplyr_1.0.1.tar.gz")'
-Rscript -e 'devtools::install_github("tidyverse/tibble")'
+Rscript -e 'remotes::install_url("https://cran.r-project.org/src/contrib/Archive/dplyr/dplyr_1.0.1.tar.gz")'
+Rscript -e 'remotes::install_github("tidyverse/tibble")'
